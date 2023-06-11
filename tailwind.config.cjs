f25d435dce38171b60b415cbd5fe8f02e2d03857
DefJunx <daniele.cencig@outlook.com>
2023-06-11 14:43:01 +0200

    Implement sidebar menu for mobile (#1)
    * Add tailwind
    
    * Implement button component and tailwind theme
    
    * remove test page
    
    * tidy up code
    
    * feat: :sparkles: Adds sidebar menu and functionalities for mobile
    
    - Adds a sidebar menu
    - Makes it animated
    
    * chore: Updates readme from template one

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "rgb(63, 81, 181)",
+        secondary: "rgb(255, 152, 0)",
+        background: "rgb(245, 245, 245)",
+        accent: "rgb(0, 188, 212)",
+        text: "rgb(51, 51, 51)"
+      },
+      height: {
+        header: "60px"
+      },
+      width: {
+        sidebar: "250px"
+      }
+    }
+  },
+  plugins: []
+};
